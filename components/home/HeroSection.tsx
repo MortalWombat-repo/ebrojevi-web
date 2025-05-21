88198a6ef4579913714533be832cd26dda7220ec
MortalWombat-repo <69204832+MortalWombat-repo@users.noreply.github.com>
2025-05-21 21:03:18 +0200

    Update HeroSection.tsx

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -10,8 +10,8 @@ import { ArrowRight } from 'lucide-react';
 import { useDropzone } from 'react-dropzone';
 
 const HeroSection = () => {
-  const containerRef = useRef<HTMLElement>(null);
-  const gridRef = useRef<HTMLElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const gridRef = useRef<HTMLDivElement>(null);
   const [image, setImage] = useState(null);
   const [error, setError] = useState('');
 
