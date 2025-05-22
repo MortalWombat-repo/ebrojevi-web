053e120d2c622159797e52097fa060575c95a540
MortalWombat-repo <69204832+MortalWombat-repo@users.noreply.github.com>
2025-05-22 13:50:36 +0200

    Updated page.tsx

--- app/database/page.tsx
+++ app/database/page.tsx
@@ -101,7 +101,7 @@ function DatabasePage() {
                     {Object.entries(item).map(([key, value]) => (
                       <TableCell key={key}>
                         {key === 'description' && value.length > 20
-                          ? `${value.substring(0, 100)}...`
+                          ? `${value.substring(0, 150)}...`
                           : value}
                       </TableCell>
                     ))}
