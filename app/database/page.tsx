3c710a865750495bbd85e80cadf4aa3878cc8678
MortalWombat-repo <69204832+MortalWombat-repo@users.noreply.github.com>
2025-05-22 10:29:27 +0200

    Updated page.tsx

--- app/database/page.tsx
+++ app/database/page.tsx
@@ -44,7 +44,7 @@ export default async function DatabasePage() {
   return (
     <div className="container mx-auto py-10 text-gray-800">
       <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Additives List</h1>
+        <h1 className="text-2xl font-bold text-muted-foreground">Additives List</h1>
         <p className="text-muted-foreground">
           Displaying all E-number additives with color-coded rows.
         </p>
